6233bd0e0d5a3609179f96f8382753ce2a9c3c45
OwenTheJayhawk <143202010+OwenTheJayhawk@users.noreply.github.com>
2023-10-15 23:10:10 -0500

    Update SalesReport.c

--- SalesReport.c
+++ SalesReport.c
@@ -1,64 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 int main() {
 
-	FILE* file;
 	
-	file = fopen("SalesReport.txt", "r");
-
-	char Report[100];
 
 	
 	
 
 
 	char months[12][30] = {"January     $", "February    $", "March       $", "April       $", "May         $", "June        $", "July        $", "August      $", "September   $", "October     $", "November    $", "December    $"};
-	double sales[12] = {};
-	float movingAverage[12];
-	float totalSales;
+	double sales[12] = {23458.01, 40112.00, 56011.85, 37820.88, 37904.67, 60200.22, 72400.31, 56210.89, 67230.84, 68233.12, 80950.34, 95225.22 };
+	
 	const char* minMonth = " "; 
 	const char* maxMonth = " ";
 
-	int i = 0;
+	int x = 0;
 	printf("%s\n", "=====================");
-	
+	printf("Monthly sales report for 2022\n");
 	printf("%s\n", "Month       Sales");
-	while (fgets(Report, 100, file)) {
-		printf("%s", months[i++]);
-		printf("%s", Report);
+	
+
+	for (int x = 0; x < 12; x++) {
+		printf("%s", months[x]);
+		printf("%.2f\n", sales[x]);
 	}
-	printf("\n%s", "=====================\n");
-
-	fclose(file);
-
-
-
+	printf("%s", "=====================\n");
 	//////////////////////////////////////////////////////////////////////
-	FILE* file2;
-	file2 = fopen("SalesReport.txt", "r");
-
-	char Report2[100];
+	
 	
 	double sum = 0.0;
 	double minimumValue = sales[0];
 	double maximumValue = sales[0];
 	int minimumPosition = 0;
 	int maximumPosition = 0;
-	for (int i = 0; i < 12; i++) {
-		if (fscanf(file2, "%lf", &sales[i]) != 1) {
-			printf("Failed to read all 12 numbers from the file.\n");
-			return 1;
-		}
-		sum += sales[i];
+	for (int x = 0; x < 12; x++) {
+		
+		sum += sales[x];
 
-		if (sales[i - 1] < minimumValue) {
-			minimumValue = sales[i];
-			minimumPosition = i;
+		if (sales[x] < minimumValue) {
+			minimumValue = sales[x];
+			minimumPosition = x;
 		}
-		if (sales[i] > maximumValue) {
-			maximumValue = sales[i];
-			maximumPosition = i;
+		if (sales[x] > maximumValue) {
+			maximumValue = sales[x];
+			maximumPosition = x;
 		}
 	}
 
@@ -157,27 +144,97 @@ int main() {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-	int monthRange;
+	
 	const char* months2[] = {
 		"January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"
 	};
 	printf("\n%s", "=====================\n");
 	printf("6-Month Moving Averages Report:\n");
-	for (int i = 0; i <= 6; i++) {
+	for (int x = 0; x <= 6; x++) {
 		double avg = 0.0;
-		for (int j = 0; j < 6; j++) {
-			avg += sales[i + j];
+		for (int y = 0; y < 6; y++) {
+			avg += sales[x + y];
 		}
 		avg /= 6;
-		printf("%s - %s: $%.2lf\n", months2[i], months2[i + 5], avg);
+		printf("%s - %s: $%.2lf\n", months2[x], months2[x + 5], avg);
 	}
 	printf("%s", "=====================\n");
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+	printf("\n%s", "=====================\n");
+	int sortedPositions[12] = {};
 
+	for (int x = 0; x < 12; x++) {
+		sortedPositions[x] = x;
+	}
 
+	for (int x = 0; x < 12 - 1; x++) {
+		for (int y = 0; y < 12 - x - 1; y++) {
+			if (sales[y] < sales[y + 1]) {
+				double temp = sales[y];
+				sales[y] = sales[y + 1];
+				sales[y + 1] = temp;
 
+				
+				int tempPosition = sortedPositions[y];
+				sortedPositions[y] = sortedPositions[y + 1];
+				sortedPositions[y + 1] = tempPosition;
+			}
+		}
+	}
+	const char* sortedMonth;
+	printf("Sales Report (Highest to Lowest)\n");
+	printf("Month       Sales\n");
+	for (int x = 0; x < 12; x++) {
+		switch (sortedPositions[x]) {
+		case 0:
+			sortedMonth = "January  ";
+			break;
+		case 1:
+			sortedMonth = "February ";
+			break;
+		case 2:
+			sortedMonth = "March    ";
+			break;
+		case 3:
+			sortedMonth = "April    ";
+			break;
+		case 4:
+			sortedMonth = "May      ";
+			break;
+		case 5:
+			sortedMonth = "June     ";
+			break;
+		case 6:
+			sortedMonth = "July     ";
+			break;
+		case 7:
+			sortedMonth = "August   ";
+			break;
+		case 8:
+			sortedMonth = "September";
+			break;
+		case 9:
+			sortedMonth = "October  ";
+			break;
+		case 10:
+			sortedMonth = "November ";
+			break;
+		case 11:
+			sortedMonth = "December ";
+			break;
+		default:
+			sortedMonth = "Error";
+			break;
 
+		}
+		printf("%s ", sortedMonth);
+		printf(" $%.2lf\n", sales[x]);
+	}
+	
+
+	printf("%s", "=====================\n");
 
 
 
